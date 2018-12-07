a172b1e7fe239f27a73a99d51b1caa0caa88dac3
MohamedAyman <mo7amed.ayman@gmail.com>
2018-12-07 20:48:05 +0200

    fix

--- frontend/app/components/home/home.controller.js
+++ frontend/app/components/home/home.controller.js
@@ -2,7 +2,7 @@ angular.module('alBargasyApp')
     .controller('homeController', function ($rootScope, $scope, $location,brandModelsService) {
         $rootScope.currentTab = "home";
           $scope.init = function(){
-            brandModelsService.getAllModelsWithoutBrand(function(res,err){
+            brandModelsService.getAllModelsWithoutBrand({},function(res,err){
                 if(!err){
                     $scope.carModels = res.data;
                     // brandModelsService.getAllExtraFeatures(function(res,err){
